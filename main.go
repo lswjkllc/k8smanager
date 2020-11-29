a525c03196dd8941a454f40dab01fd5f1e2fbbb6
lswjkllc <lswjkllc@163.com>
2020-11-30 00:58:37 +0800

    [feature]连接k8s

--- main.go
+++ main.go
@@ -1,24 +1,39 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
-	// "k8s.io/client-go/tools/clientcmd"
-)
 
-// out of cluster mode
-var (
-	kubeConfig = flag.String(
-		"kubeconfig",
-		"./config.yml",
-		"absolute path to the kubelet config file")
+	apiv1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
 func main() {
-	fmt.Println("Beginning...")
+	var kubeconfig *string
+	kubeconfig = flag.String("kubeconfig", "./config.yaml", "absolute path to the kubeconfig file")
+	flag.Parse()
 
-	// config, err := clientcmd.BuildConfigFromFlags("", *kubeConfig)
-	// if err != nil {
-	// 	fmt.Println("Get k8s config failed %#v", err)
-	// }
+	// use the current context in kubeconfig
+	config, err := clientcmd.BuildConfigFromFlags("42.192.221.6:6443", *kubeconfig)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// create the clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	deploymentsClient := clientset.AppsV1().Deployments(apiv1.NamespaceDefault)
+	list, err := deploymentsClient.List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		panic(err)
+	}
+	for _, d := range list.Items {
+		fmt.Printf(" * %s (%d replicas)\n", d.Name, *d.Spec.Replicas)
+	}
 }
