c8b9c409d88b936b784706c243e784b14ff8a1ba
Mustom <liucheng@tuya.com>
2020-12-01 09:50:47 +0800

    [feature]连接集群，获取pod

--- main.go
+++ main.go
@@ -4,20 +4,35 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"time"
 
-	apiv1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+	//
+	// Uncomment to load all auth plugins
+	// _ "k8s.io/client-go/plugin/pkg/client/auth"
+	//
+	// Or uncomment to load specific auth plugins
+	// _ "k8s.io/client-go/plugin/pkg/client/auth/azure"
+	// _ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+	// _ "k8s.io/client-go/plugin/pkg/client/auth/oidc"
+	// _ "k8s.io/client-go/plugin/pkg/client/auth/openstack"
 )
 
 func main() {
 	var kubeconfig *string
-	kubeconfig = flag.String("kubeconfig", "./config.yaml", "absolute path to the kubeconfig file")
+	if home := homedir.HomeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", "./config.yaml", "(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "./config.yaml", "absolute path to the kubeconfig file")
+	}
 	flag.Parse()
 
 	// use the current context in kubeconfig
-	config, err := clientcmd.BuildConfigFromFlags("42.192.221.6:6443", *kubeconfig)
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
 	if err != nil {
 		panic(err.Error())
 	}
@@ -27,13 +42,43 @@ func main() {
 	if err != nil {
 		panic(err.Error())
 	}
-
-	deploymentsClient := clientset.AppsV1().Deployments(apiv1.NamespaceDefault)
-	list, err := deploymentsClient.List(context.TODO(), metav1.ListOptions{})
+	// get nodes
+	nodes, err := clientset.CoreV1().Nodes().List(context.TODO(), metav1.ListOptions{})
 	if err != nil {
-		panic(err)
+		fmt.Printf("%v", err)
+		return
 	}
-	for _, d := range list.Items {
-		fmt.Printf(" * %s (%d replicas)\n", d.Name, *d.Spec.Replicas)
+	fmt.Printf("There are %d nodes in the cluster\n", len(nodes.Items))
+	for i, node := range nodes.Items {
+		fmt.Println(i, node.Name)
+	}
+
+	// get pods
+	pods, err := clientset.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
+	for i, pod := range pods.Items {
+		fmt.Println(i, pod.Name)
+	}
+
+	// cycle get the pod
+	for {
+		namespace := "default"
+		pod := "nginx"
+		_, err = clientset.CoreV1().Pods(namespace).Get(context.TODO(), pod, metav1.GetOptions{})
+		if errors.IsNotFound(err) {
+			fmt.Printf("Pod %s in namespace %s not found\n", pod, namespace)
+		} else if statusError, isStatus := err.(*errors.StatusError); isStatus {
+			fmt.Printf("Error getting pod %s in namespace %s: %v\n",
+				pod, namespace, statusError.ErrStatus.Message)
+		} else if err != nil {
+			panic(err.Error())
+		} else {
+			fmt.Printf("Found pod {%s} in namespace {%s}\n", pod, namespace)
+		}
+
+		time.Sleep(3 * time.Second)
 	}
 }
