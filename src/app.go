aae9860ac0aed5bdb5d58131f92d486109abe55a
lswjkllc <lswjkllc@163.com>
2020-12-06 21:45:45 +0800

    [feature]web绑定

--- src/app.go
+++ src/app.go
@@ -1,14 +1,53 @@
 package main
 
 import (
-	api "k8smanager/src/handlers"
+	"k8smanager/src/handlers"
+	"net/http"
 
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
+
+func addRouter(e *echo.Echo) {
+	e.POST("/users", handlers.SaveUser)
+	e.GET("/users/:id", handlers.GetUser)
+	e.GET("/show", handlers.Show)
+	e.POST("/save", handlers.Save)
+	e.POST("/save/file", handlers.SaveFile)
+}
+
+func addMiddleware(e *echo.Echo) {
+	// Root level middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Group level middleware
+	g := e.Group("/admin")
+	g.Use(middleware.BasicAuth(func(username, password string, c echo.Context) (bool, error) {
+		if username == "joe" && password == "secret" {
+			return true, nil
+		}
+		return false, nil
+	}))
+
+	// Route level middleware
+	track := func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			println("request to /users")
+			return next(c)
+		}
+	}
+	e.GET("/users", func(c echo.Context) error {
+		return c.String(http.StatusOK, "/users")
+	}, track)
+}
 
 func main() {
 	e := echo.New()
-	e.GET("/user/:id", api.GetUser)
+
+	addMiddleware(e)
+	// addRouter(e)
+
 	e.Logger.Fatal(e.Start(":1323"))
 
 	// api.ConnectK8S("./config/config.yaml")
---
+++ src/models/user.go
@@ -0,0 +1,6 @@
+package models
+
+type User struct {
+	Name  string `json:"name" xml:"name" form:"name" query:"name"`
+	Email string `json:"email" xml:"email" form:"email" query:"email"`
+}
