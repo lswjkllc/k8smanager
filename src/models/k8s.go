69ef0f4323586d6c5f617c4f72553d087bbad183
Mustom <liucheng@tuya.com>
2020-12-09 20:33:39 +0800

    [feature]DeploymentParams增加VolumeMounts字段

--- src/models/k8s.go
+++ src/models/k8s.go
@@ -76,11 +76,19 @@ type EnvVar struct {
 	Value string `json:"value" xml:"value" form:"value" query:"value"`
 }
 
+type VolumeMount struct {
+	Name      string `json:"name" xml:"name" form:"name" query:"name"`
+	ReadOnly  bool   `json:"readOnly" xml:"readOnly" form:"readOnly" query:"readOnly"`
+	MountPath string `json:"mountPath" xml:"mountPath" form:"mountPath" query:"mountPath"`
+	SubPath   string `json:"subPath" xml:"subPath" form:"subPath" query:"subPath"`
+}
+
 type DeploymentParams struct {
-	Name      string               `json:"name" xml:"name" form:"name" query:"name"`
-	Namespace string               `json:"namespace" xml:"namespace" form:"namespace" query:"namespace"`
-	Image     string               `json:"image" xml:"image" form:"image" query:"image"`
-	Resources ResourceRequirements `json:"resources" xml:"resources" form:"resources" query:"resources"`
-	Replicas  int32                `json:"replicas" xml:"replicas" form:"replicas" query:"replicas"`
-	Env       []EnvVar             `json:"env" xml:"env" form:"env" query:"env"`
+	Name         string               `json:"name" xml:"name" form:"name" query:"name"`
+	Namespace    string               `json:"namespace" xml:"namespace" form:"namespace" query:"namespace"`
+	Image        string               `json:"image" xml:"image" form:"image" query:"image"`
+	Resources    ResourceRequirements `json:"resources" xml:"resources" form:"resources" query:"resources"`
+	Replicas     int32                `json:"replicas" xml:"replicas" form:"replicas" query:"replicas"`
+	Env          []EnvVar             `json:"env" xml:"env" form:"env" query:"env"`
+	VolumeMounts []VolumeMount        `json:"volumeMounts" xml:"volumeMounts" form:"volumeMounts" query:"volumeMounts"`
 }
